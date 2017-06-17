7e47edd7b7a495f221200d68b8366e93684f77a4
Jaromil <jaromil@dyne.org>
2017-06-17 17:22:56 +0200

    removed gorilla repl
    also added midje among deps for testing

--- project.clj
+++ project.clj
@@ -18,19 +18,6 @@
                            [markdown-clj "0.9.98"]
                            [json-html "0.4.0"]
 
-                           ;; gorilla-repl deps
-                           [http-kit "2.2.0"]
-                           [ring/ring-json "0.4.0"]
-                           ;; [compojure "1.5.1"]
-                           [org.slf4j/slf4j-api "1.7.22"]
-                           [ch.qos.logback/logback-classic "1.1.8"]
-                           [gorilla-renderable "2.0.0"]
-                           [gorilla-plot "0.1.4"]
-                           [javax.servlet/servlet-api "2.5"]
-                           [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
-                           [cider/cider-nrepl "0.14.0"]
-                           [org.clojure/tools.nrepl "0.2.12"]
-
                            ;; spreadsheet
                            [dk.ative/docjure "1.11.0"]
                            [org.apache.poi/poi "3.15"]
@@ -44,7 +31,11 @@
   :resource-paths ["resources"]
   :template-additions ["ws/index.clj"]
   :main ^:skip-aot agiladmin.core
-  :profiles {:uberjar {:aot [gorilla-repl.core agiladmin.core]}}
+  :profiles {
+             :uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}}
+
   :target-path "target/%s"
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler agiladmin.handlers/app}
