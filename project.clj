fe2a10e5a88916363312c45abd098eceea68602d
Jaromil <jaromil@dyne.org>
2017-06-22 15:42:27 +0200

    update all library deps

--- project.clj
+++ project.clj
@@ -4,25 +4,25 @@
   :license {:name "Affero GPL v3"}
   :dependencies ^:replace [[org.clojure/clojure "1.8.0"]
                            [org.clojure/data.json "0.2.6"]
-                           [org.clojure/data.csv "0.1.3"]
-                           [org.clojure/tools.nrepl "0.2.12"]
-                           [clj-http "3.4.1"]
-                           [cheshire "5.6.3"]
+                           [org.clojure/data.csv "0.1.4"]
+                           [org.clojure/tools.nrepl "0.2.13"]
+                           [clj-http "3.6.1"]
+                           [cheshire "5.7.1"]
                            [clojure-humanize "0.2.2"]
 
                            ;; compojure, ring and middleware
-                           [compojure "1.5.2"]
-                           [ring/ring-defaults "0.2.3"]
+                           [compojure "1.6.0"]
+                           [ring/ring-defaults "0.3.0"]
                            [ring-middleware-accept "2.0.3"]
                            ;; aux web stuff
                            [formidable "0.1.10"]
-                           [markdown-clj "0.9.98"]
-                           [json-html "0.4.0"]
+                           [markdown-clj "0.9.99"]
+                           [json-html "0.4.4"]
 
                            ;; spreadsheet
                            [dk.ative/docjure "1.11.0"]
-                           [org.apache.poi/poi "3.15"]
-                           [org.apache.poi/poi-ooxml "3.15"]
+                           [org.apache.poi/poi "3.16"]
+                           [org.apache.poi/poi-ooxml "3.16"]
 
                            ;; git
                            [clj-jgit "0.9.1-SNAPSHOT"]
