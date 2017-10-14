0d08a56cd2f2f8c1603c66ca8fa836f5722ce3cf
Jaromil <jaromil@dyne.org>
2017-10-14 12:58:01 +0200

    minor configuration tests

--- test/agiladmin/config_test.clj
+++ test/agiladmin/config_test.clj
@@ -1,8 +1,16 @@
-
 (ns agiladmin.config-test
-  (:require [agiladmin.config :as conf]
+  (:use midje.sweet)
+  (:require [auxiliary.config :as aux]
+            [agiladmin.config :as conf]
             [clojure.pprint :refer [pprint]]))
 
-(def conf (conf/load-config "agiladmin" conf/default-settings))
-; (pprint (conf/load "agiladmin" conf/default-settings))
-(conf/load-project conf "decode")
+(fact "Configuration load"
+      (let [conf (aux/yaml-read "test/assets/agiladmin.yaml")]
+        (pprint "Test config:")
+        (pprint conf)
+        conf => {:agiladmin {:git "https://github.com/dyne/agiladmin", :update true}, :budgets {:git "ssh://dyne.org/dyne/budgets", :path "budgets", :ssh-key "id_rsa"}, :projects ["proj_uno" "proj_due" "proj_tre"]}
+
+        (fact "Project configuration load"
+              (let [proj (aux/yaml-read "test/assets/proj_uno.yaml")]
+                (pprint "Test project config:")
+                (pprint proj)))))
