796b0b660b070cbd0bae4458041fd8383f70e0af
Jaromil <jaromil@dyne.org>
2017-10-15 13:44:50 +0200

    tidying up config test

--- test/agiladmin/config_test.clj
+++ test/agiladmin/config_test.clj
@@ -6,8 +6,6 @@
 
 (fact "Configuration load"
       (let [conf (aux/yaml-read "test/assets/agiladmin.yaml")]
-        (pprint "Test config:")
-        (pprint conf)
         conf => {:agiladmin {:budgets {:git "ssh://dyne.org/dyne/budgets", :path "test/assets/", :ssh-key "id_rsa"}, :projects ["UNO" "DUE" "TRE"], :source {:git "https://github.com/dyne/agiladmin", :update true}}}
 
         (fact "Project configuration load"
