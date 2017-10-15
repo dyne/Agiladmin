d68bed5f3d1a4c573f340cb4dc88ef11b1f6c2c1
Jaromil <jaromil@dyne.org>
2017-10-15 11:13:07 +0200

    config test fix

--- test/agiladmin/config_test.clj
+++ test/agiladmin/config_test.clj
@@ -8,7 +8,7 @@
       (let [conf (aux/yaml-read "test/assets/agiladmin.yaml")]
         (pprint "Test config:")
         (pprint conf)
-        conf => {:agiladmin {:source {:git "https://github.com/dyne/agiladmin", :update true}, :budgets {:git "ssh://dyne.org/dyne/budgets", :path "test/assets/", :ssh-key "id_rsa"}, :projects ["proj_uno" "proj_due" "proj_tre"]}}
+        conf => {:agiladmin {:budgets {:git "ssh://dyne.org/dyne/budgets", :path "test/assets/", :ssh-key "id_rsa"}, :projects ["UNO" "DUE" "TRE"], :source {:git "https://github.com/dyne/agiladmin", :update true}}}
 
         (fact "Project configuration load"
               (let [proj (aux/yaml-read "test/assets/proj_uno.yaml")]
