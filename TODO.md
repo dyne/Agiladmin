3c072ab6d5a14d2806c29e68f8dc6c042703e2b5
Jaromil <jaromil@dyne.org>
2018-12-06 12:57:43 +0100

    updated TODO analysis with Manuela
    also reformatted view reload

--- TODO.md
+++ TODO.md
@@ -1,10 +1,53 @@
 
+# Bug fix
+
+- error after upload of timesheet
+- disappearance T5.1 on DECODE upon ts upload
+- mark logged username in git history
+- remove email passwords from configuration!!!!
+
+# Manuela's feedback and reminders
+
+- bottoni di bootstrap non clickabili con tasto destro per new tab
+- prima pagina se loggato deve dare la dashboard e non "login"
+- un search per la lista dei nomi per quando diventano tanti
+- logo e link di dyne metterlo in alto
+- nel footer mettere i nostri contatti
+- newcomers da capire dove mettere con il design
+- lista persone con accanto link agli anni precedenti
+  e anche ai progetti in cui partecipa <- nuova view!
+	  view della persona sul progetto (solo per quel progetto)
+	  con una view praticamente uguale a quella sulla persona
+		(reuse person_view)
+- nella person_view al posto dello zero 0 mettere un trattino -
+  per quelle voci che sono VOL
+- Yearly totals nel person_view
+  aggiungere sotto anche la specifica sui totali per progetto
+  cioe' totale annuale e anche sui progetti
+- Project_view
+  aggiungere accanto al progetto i singoli anni
+  anni farli clickabili che visualizzino solo i dati di quell'anno
+  un po' come per le persone (reuse project_view) passandogli un
+  dataset limitato a quell'anno
+
+
+
+- previous year button also on top of person's page
+- reformulate monthly report to exclude voluntary hours
+- button to set month as paid and lock total modifications
+- per-project voluntary hours separated from other hours
+  they are all mixed with paid hours
+- per-project yearly totals
+
+
+
 # General Improvements
 
 - push on succesfull upload of timesheet
 - save cached parsing of timesheet in clj-storage nippy
-- add user authentication via just-auth
+X add user authentication via just-auth
 - show unused tasks into project listings
+- yearly voluntary hours per person
 
 # Timesheet Improvements
 
@@ -18,6 +61,7 @@
 
 # JS components of interest
 
+- https://github.com/plouc/nivo (D3 based dataviz graphs)
 
 - https://dhtmlx.com/docs/products/dhtmlxSuite/
 
--- src/agiladmin/view_reload.clj
+++ src/agiladmin/view_reload.clj
@@ -19,19 +19,19 @@
 
 (ns agiladmin.view-reload
   (:require
-   [clojure.java.io :as io]
-   [agiladmin.utils :as util]
-   [agiladmin.webpage :as web]
-   [agiladmin.config :as conf]
-   [taoensso.timbre :as log]
-   [clj-jgit.porcelain :as git
-    :refer [with-identity load-repo git-clone git-pull]]))
+    [clojure.java.io :as io]
+    [agiladmin.utils :as util]
+    [agiladmin.webpage :as web]
+    [agiladmin.config :as conf]
+    [taoensso.timbre :as log]
+    [clj-jgit.porcelain :as git
+     :refer [with-identity load-repo git-clone git-pull]]))
 
 (defn start [request config account]
   (let [budgets (conf/q config [:agiladmin :budgets])
         keypath (:ssh-key budgets)
         gitpath (:git budgets)
-        path    (io/file (:path budgets))]
+        path (io/file (:path budgets))]
     (cond
       (.isDirectory path)
       ;; the directory exists (we assume also is a correct
@@ -42,58 +42,58 @@
                       (catch Exception ex
                         (log/error [:p "Error in git/load-repo: " ex])))]
         (log/info
-         (str "Path is a directory, trying to pull in: "
-              (:path budgets)))
+          (str "Path is a directory, trying to pull in: "
+               (:path budgets)))
 
         (web/render
-         account
-         [:div {:class "container-fluid"}
-          (git/with-identity {:name (:ssh-key budgets)
-                              :passphrase ""
-                              :exclusive true}
-            (let [res (try (git/git-pull repo)
-                           (catch Exception ex
-                             (web/render-error
-                              (log/spy :error [:div [:p (str "Error in git-pull: " (.getMessage ex))]
-                                               [:p (-> ex Throwable->map :cause)]]))))]
-              (if (= (type res) org.eclipse.jgit.api.PullResult)
-                [:div {:class "alert alert-success"}
-                 (str "Reloaded successfully from " (:git budgets))]
-                res)))
+          account
+          [:div {:class "container-fluid"}
+           (git/with-identity {:name       (:ssh-key budgets)
+                               :passphrase ""
+                               :exclusive  true}
+                              (let [res (try (git/git-pull repo)
+                                             (catch Exception ex
+                                               (web/render-error
+                                                 (log/spy :error [:div [:p (str "Error in git-pull: " (.getMessage ex))]
+                                                                  [:p (-> ex Throwable->map :cause)]]))))]
+                                (if (= (type res) org.eclipse.jgit.api.PullResult)
+                                  [:div {:class "alert alert-success"}
+                                   (str "Reloaded successfully from " (:git budgets))]
+                                  res)))
 
-          [:div [:h1 "Git status"]
-           (web/render-yaml (git/git-status repo))]
-          [:div {:class "col-md-6"} [:h1 "Log (last 20 changes)"]
-           (web/render-git-log repo)]
-          [:div {:class "col-md-6"}  [:h1 "Config"] (web/render-yaml config)]
-          ]))
+           [:div [:h1 "Git status"]
+            (web/render-yaml (git/git-status repo))]
+           [:div {:class "col-md-6"} [:h1 "Log (last 20 changes)"]
+            (web/render-git-log repo)]
+           [:div {:class "col-md-6"} [:h1 "Config"] (web/render-yaml config)]
+           ]))
 
 
       (.exists path)
       ;; exists but is not a directory
       (web/render-error-page
-       config
-       (log/spy
-        :error
-        (str "Invalid budgets directory: " (:path budgets))))
+        config
+        (log/spy
+          :error
+          (str "Invalid budgets directory: " (:path budgets))))
 
       :else
       ;; doesn't exists at all
       (web/render
        account
        [:div {:class "container-fluid"}
-        (git/with-identity {:name keypath
+        (git/with-identity {:name       keypath
                             ;; :private (slurp keypath)
                             ;; :public  (slurp (str keypath ".pub")
                             :passphrase ""
-                            :exclusive true}
-          (try (git/git-clone (:path budgets) "budgets")
-               (catch Exception ex
-                 (web/render-error
-                  (log/spy :error
-                           [:p "Error cloning git repo" ex
-                            "Add your public key to the repository to access it:"
-                            (-> (str keypath ".pub") slurp str)])))))
+                            :exclusive  true}
+                           (try (git/git-clone (:path budgets) "budgets")
+                                (catch Exception ex
+                                  (web/render-error
+                                    (log/spy :error
+                                             [:p "Error cloning git repo" ex
+                                              "Add your public key to the repository to access it:"
+                                              (-> (str keypath ".pub") slurp str)])))))]
 
         (if-let [repo (git/load-repo (:path budgets))]
           [:div
@@ -101,6 +101,6 @@
             (web/render-yaml (git/git-status repo))]
            [:div {:class "col-md-6"} [:h1 "Log (last 20 changes)"]
             (web/render-git-log repo)]
-           [:div {:class "col-md-6"}  [:h1 "Config"] (web/render-yaml config)]])])
+           [:div {:class "col-md-6"} [:h1 "Config"] (web/render-yaml config)]]))
       ;; end of POST /reload
       )))
