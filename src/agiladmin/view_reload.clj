bc2dfbecc2ff73bae5b28672d35f37ec13e027db
Jaromil <jaromil@dyne.org>
2026-03-09 23:31:58 +0100

    fix: handle reload without budgets repository

--- src/agiladmin/view_reload.clj
+++ src/agiladmin/view_reload.clj
@@ -19,54 +19,94 @@
 
 (ns agiladmin.view-reload
   (:require
-    [clojure.java.io :as io]
-    [agiladmin.utils :as util]
-    [agiladmin.webpage :as web]
-    [agiladmin.config :as conf]
-    [taoensso.timbre :as log]
-    [clj-jgit.porcelain :as git
-     :refer [with-identity load-repo git-clone git-pull]]))
+   [clojure.java.io :as io]
+   [agiladmin.webpage :as web]
+   [agiladmin.config :as conf]
+   [taoensso.timbre :as log]
+   [clj-jgit.porcelain :as git
+    :refer [with-identity load-repo git-clone git-pull]]))
+
+(defn- render-reload-message
+  [account message]
+  (web/render
+   account
+   [:div {:class "container-fluid"}
+    [:div {:class "alert alert-info"}
+     message]]))
+
+(defn- render-reload-error
+  [account message]
+  (web/render
+   account
+   [:div {:class "container-fluid"}
+    (web/render-error message)]))
+
+(defn- git-ready?
+  [budgets]
+  (every? seq [(:path budgets) (:git budgets) (:ssh-key budgets)]))
+
+(defn- safe-load-repo
+  [path]
+  (try
+    (git/load-repo path)
+    (catch Exception ex
+      (log/error [:p "Error in git/load-repo: " ex])
+      nil)))
+
+(defn- render-repo-state
+  [account repo]
+  (web/render
+   account
+   [:div {:class "container-fluid"}
+    [:div [:h1 "Git status"]
+     (web/render-yaml (git/git-status repo))]
+    [:div [:h1 "Log (last 20 changes)"]
+     (web/render-git-log repo)]]))
 
 (defn start [request config account]
   (let [budgets (conf/q config [:agiladmin :budgets])
         keypath (:ssh-key budgets)
-        gitpath (:git budgets)
         path (io/file (:path budgets))]
     (cond
+      (not (git-ready? budgets))
+      (render-reload-message
+       account
+       "Reload is unavailable until :agiladmin :budgets has git, path, and ssh-key configured.")
+
       (.isDirectory path)
       ;; the directory exists (we assume also is a correct
       ;; one) TODO: analyse contents of path, detect git
       ;; repo and correct agiladmin environment, detect
       ;; errors and report them
-      (let [repo (try (git/load-repo (:path budgets))
-                      (catch Exception ex
-                        (log/error [:p "Error in git/load-repo: " ex])))]
-        (log/info
-          (str "Path is a directory, trying to pull in: "
-               (:path budgets)))
-
-        (web/render
-          account
-          [:div {:class "container-fluid"}
-           (git/with-identity {:name       (:ssh-key budgets)
-                               :passphrase ""
-                               :exclusive  true}
-                              (let [res (try (git/git-pull repo)
-                                             (catch Exception ex
-                                               (web/render-error
-                                                 (log/spy :error [:div [:p (str "Error in git-pull: " (.getMessage ex))]
-                                                                  [:p (-> ex Throwable->map :cause)]]))))]
-                                (if (= (type res) org.eclipse.jgit.api.PullResult)
-                                  [:div {:class "alert alert-success"}
-                                   (str "Reloaded successfully from " (:git budgets))]
-                                  res)))
-
-           [:div [:h1 "Git status"]
-            (web/render-yaml (git/git-status repo))]
-           [:div [:h1 "Log (last 20 changes)"]
-            (web/render-git-log repo)]
-           ;; [:div {:class "col-md-6"} [:h1 "Config"] (web/render-yaml config)]
-           ]))
+      (if-let [repo (safe-load-repo (:path budgets))]
+        (do
+          (log/info
+           (str "Path is a directory, trying to pull in: "
+                (:path budgets)))
+          (web/render
+           account
+           [:div {:class "container-fluid"}
+            (git/with-identity {:name (:ssh-key budgets)
+                                :passphrase ""
+                                :exclusive true}
+                               (let [res (try (git/git-pull repo)
+                                              (catch Exception ex
+                                                (web/render-error
+                                                 (log/spy :error
+                                                          [:div
+                                                           [:p (str "Error in git-pull: " (.getMessage ex))]
+                                                           [:p (-> ex Throwable->map :cause)]]))))]
+                                 (if (= (type res) org.eclipse.jgit.api.PullResult)
+                                   [:div {:class "alert alert-success"}
+                                    (str "Reloaded successfully from " (:git budgets))]
+                                   res)))
+            [:div [:h1 "Git status"]
+             (web/render-yaml (git/git-status repo))]
+            [:div [:h1 "Log (last 20 changes)"]
+             (web/render-git-log repo)]]))
+        (render-reload-message
+         account
+         (str "Budgets path exists but is not a git repository yet: " (:path budgets))))
 
 
       (.exists path)
@@ -79,26 +119,32 @@
 
       :else
       ;; doesn't exists at all
-      (web/render
-       account
-       [:div {:class "container-fluid"}
-        (git/with-identity {:name       keypath
-                            ;; :private (slurp keypath)
-                            ;; :public  (slurp (str keypath ".pub")
-                            :passphrase ""
-                            :exclusive  true}
-                           (try (git/git-clone (:path budgets) "budgets")
-                                (catch Exception ex
-                                  (web/render-error
-                                    (log/spy :error
-                                             [:p "Error cloning git repo" ex
-                                              "Add your public key to the repository to access it:"
-                                              (-> (str keypath ".pub") slurp str)])))))]
-
-        (if-let [repo (git/load-repo (:path budgets))]
-          [:div
-           [:div [:h1 "Git status"]
-            (web/render-yaml (git/git-status repo))]
-           [:div [:h1 "Log (last 20 changes)"] (web/render-git-log repo)]]))
+      (if (.exists (io/file (str keypath ".pub")))
+        (let [clone-result
+              (git/with-identity {:name keypath
+                                  :passphrase ""
+                                  :exclusive true}
+                                 (try (git/git-clone (:path budgets) "budgets")
+                                      (catch Exception ex
+                                        (web/render-error
+                                         (log/spy :error
+                                                  [:p "Error cloning git repo" ex
+                                                   "Add your public key to the repository to access it:"
+                                                   (-> (str keypath ".pub") slurp str)])))))]
+          (web/render
+           account
+           [:div {:class "container-fluid"}
+            clone-result
+            (if-let [repo (safe-load-repo (:path budgets))]
+              [:div
+               [:div [:h1 "Git status"]
+                (web/render-yaml (git/git-status repo))]
+               [:div [:h1 "Log (last 20 changes)"] (web/render-git-log repo)]]
+              [:div {:class "alert alert-info"}
+               (str "No budgets repository is available yet at " (:path budgets))])]))
+        (render-reload-message
+         account
+         (str "No budgets repository is available yet. Generate or configure SSH keys first: "
+              keypath ".pub")))
       ;; end of POST /reload
       )))
