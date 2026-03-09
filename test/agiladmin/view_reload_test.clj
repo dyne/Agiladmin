a24ee98bed79330cc243ed8be8cc3155ad8c8b4a
Jaromil <jaromil@dyne.org>
2026-03-09 23:31:58 +0100

    fix: handle reload without budgets repository

---
+++ test/agiladmin/view_reload_test.clj
@@ -0,0 +1,26 @@
+(ns agiladmin.view-reload-test
+  (:require [agiladmin.view-reload :as view-reload]
+            [midje.sweet :refer :all]))
+
+(fact "Reload page explains missing budgets git configuration"
+      (let [response (view-reload/start
+                      {}
+                      {:agiladmin {:budgets {:path "budgets/"}}}
+                      {:email "admin"})]
+        (:body response) => (contains "Reload is unavailable until :agiladmin :budgets has git, path, and ssh-key configured.")))
+
+(fact "Reload page explains when the budgets path is not a git repository"
+      (with-redefs [clojure.java.io/file
+                    (fn [_]
+                      (proxy [java.io.File] ["budgets"]
+                        (isDirectory [] true)
+                        (exists [] true)))
+                    agiladmin.view-reload/safe-load-repo
+                    (fn [_] nil)]
+        (let [response (view-reload/start
+                        {}
+                        {:agiladmin {:budgets {:path "budgets/"
+                                               :git "git@example.org:repo.git"
+                                               :ssh-key "id_rsa"}}}
+                        {:email "admin"})]
+          (:body response) => (contains "Budgets path exists but is not a git repository yet: budgets/"))))
