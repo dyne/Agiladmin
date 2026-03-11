fa758484c7b2a8bfac19d7cb17926bb969c6fd6b
Jaromil <jaromil@dyne.org>
2026-03-11 07:55:05 +0100

    docs: track pocketbase admin field

---
+++ pb_migrations/1773211470_updated_users.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "hidden": false,
+    "id": "bool2282622326",
+    "name": "admin",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "bool"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove field
+  collection.fields.removeById("bool2282622326")
+
+  return app.save(collection)
+})
