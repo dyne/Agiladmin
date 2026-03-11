7dda815956937c8464b4797027a1b04cc7b5fcb6
Jaromil <jaromil@dyne.org>
2026-03-11 21:03:02 +0100

    build: add Tailwind, DaisyUI, and HTMX frontend assets

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.clj",
+    "./resources/**/*.html",
+    "./resources/**/*.js"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["nord"]
+  }
+};
