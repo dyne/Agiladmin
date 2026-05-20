d6ac603c1621584ffc8fb0d5ee94998c95092bdc
Jaromil <jaromil@dyne.org>
2026-05-20 09:30:56 +0200

    feat(upload): render progress UI in timesheet upload form

--- test/agiladmin/view_timesheet_test.clj
+++ test/agiladmin/view_timesheet_test.clj
@@ -11,6 +11,8 @@
         html => (contains "hx-post=\"/timesheets/upload\"")
         html => (contains "id=\"timesheet-workspace\"")
         html => (contains "class=\"flex items-end gap-3\"")
+        html => (contains "data-upload-progress=\"true\"")
+        html => (contains "data-upload-progress-label=\"true\"")
         html => (contains "Uploading and validating timesheet...")
         html => (contains "shrink-0")))
 
